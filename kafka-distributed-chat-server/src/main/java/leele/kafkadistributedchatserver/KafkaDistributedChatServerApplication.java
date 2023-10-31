fe702c164d4783226506069b8dea6fd7271604bc
younglee-Choe <leele7111@naver.com>
2023-10-31 17:46:11 +0900

    main application

---
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/KafkaDistributedChatServerApplication.java
@@ -0,0 +1,12 @@
+package leele.kafkadistributedchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDistributedChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDistributedChatServerApplication.class, args);
+    }
+}
