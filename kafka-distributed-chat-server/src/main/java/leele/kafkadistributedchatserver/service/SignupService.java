49bae9ef0b56060968100f9494442f70d4985f0b
younglee-Choe <leele7111@naver.com>
2023-10-31 17:41:27 +0900

    service for registering users in DB and registers an encrypted password

---
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/service/SignupService.java
@@ -0,0 +1,36 @@
+package leele.kafkadistributedchatserver.service;
+
+import leele.kafkadistributedchatserver.member.entity.Member;
+import leele.kafkadistributedchatserver.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SignupService {
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public Member register(Member params) {
+        String encryptedPassword = passwordEncoder.encode(params.getPassword());
+
+        Member member = Member.builder()
+                .memberId(params.getMemberId())
+                .password(encryptedPassword)
+                .name(params.getName())
+                .role("ROLE_MEMBER")
+                .joinDate(LocalDateTime.now())
+                .build();
+
+        return memberRepository.save(member);
+    }
+}
