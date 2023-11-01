e28d1c362745eb0a7bf7ef1a7c8f8de36b234571
younglee-Choe <leele7111@naver.com>
2023-11-01 14:54:23 +0900

    root router

--- kafka-distributed-chat-client/src/App.jsx
+++ kafka-distributed-chat-client/src/App.jsx
@@ -6,6 +6,7 @@ import LogIn from './components/auth/LogIn';
 import SignUp from './components/auth/SignUp';
 import CreateRoom from './components/room/CreateRoom';
 import RoomList from './components/room/RoomList';
+import SearchRoom from './components/room/SearchRoom';
 import ChatPage from './components/chat/ChatPage';
 
 function App() {
@@ -21,19 +22,24 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div>
-          {isSession ?
-            <Header />
-            :
-            <></>
-          }
-          <Routes>
-            <Route path="/room/createRoom" element={<CreateRoom/>}></Route>
-            <Route path="/room/roomList" element={<RoomList/>}></Route>
-            <Route path="/chat" element={<ChatPage/>}></Route>
-            <Route path="/signup" element={<SignUp/>}></Route>
-            <Route path="/" element={<LogIn/>}></Route>
-          </Routes>
+        <div className="app__main">
+          <div className="app__header">
+            {isSession ?
+              <Header />
+              :
+              <></>
+            }
+          </div>
+          <div className="app__body">
+            <Routes>
+              <Route path="/room/createRoom" element={<CreateRoom/>}></Route>
+              <Route path="/room/roomList" element={<RoomList/>}></Route>
+              <Route path="/room/searchRoom" element={<SearchRoom/>}></Route>
+              <Route path="/chat" element={<ChatPage/>}></Route>
+              <Route path="/signup" element={<SignUp/>}></Route>
+              <Route path="/" element={<LogIn/>}></Route>
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
