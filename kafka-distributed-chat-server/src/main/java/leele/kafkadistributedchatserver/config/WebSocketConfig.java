cb57020c816c0a9dff3aff917ebdcadf495e060f
younglee-Choe <leele7111@naver.com>
2023-11-02 09:35:16 +0900

    add STOMP configuration

--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebSocketConfig.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebSocketConfig.java
@@ -1,21 +1,22 @@
 package leele.kafkadistributedchatserver.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-@RequiredArgsConstructor
 @Configuration
-@EnableWebSocket    // WebSocket 활성화
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
+@EnableWebSocketMessageBroker   // STOMP 활성화
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws/chat")    // WebSocket server endpoint
-                    .setAllowedOrigins("*");    // 허용할 url 지정 (CORS방지)
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");    // 클라이언트에게 메시지 보낼 수 있도록 Simple Broker 등록 ("/queue")
+        registry.setApplicationDestinationPrefixes("/pub");          // @MessageMapping이 붙은 method로 바운드됨 ("/topic")
     }
 }
