a127e60aa45214160fe4b7cc88b5c9b99179db05
younglee-Choe <leele7111@naver.com>
2023-11-05 04:31:42 +0900

    read system environment variables; value protection

--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebMvcConfig.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package leele.kafkadistributedchatserver.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,11 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${client.origin}")
+    private String origin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(origin)
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebSocketConfig.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package leele.kafkadistributedchatserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -7,11 +8,13 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker   // STOMP 활성화
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${client.origin}")
+    private String origin;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000");
+                .setAllowedOrigins(origin);
     }
 
     @Override
