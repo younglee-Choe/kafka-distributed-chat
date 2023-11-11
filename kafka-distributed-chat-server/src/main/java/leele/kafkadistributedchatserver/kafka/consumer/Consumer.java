76999ee7fd44086695730c34d1a20efb8c472b10
younglee-Choe <leele7111@naver.com>
2023-11-12 05:54:13 +0900

    after reading the message from Kafka for the request, close the connection with Kafka

--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/kafka/consumer/Consumer.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/kafka/consumer/Consumer.java
@@ -4,12 +4,16 @@ import leele.kafkadistributedchatserver.chat.dto.Chat;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import java.util.*;
 
+@RestController
 public class Consumer {
     @Value("${kafka.brokers}")
     private static String brokers;
@@ -17,28 +21,66 @@ public class Consumer {
     @Value("${kafka.group.id}")
     private static String groupId;
 
-    public static void consume(String topic) {
+    private static SimpMessagingTemplate messagingTemplate;
+
+    public static void consume(String roomId, String memberId) {
         Properties props = new Properties();
         props.put("bootstrap.servers", brokers);
         props.put("group.id", groupId);
+        props.put("enable.auto.commit", "false");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "earliest");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "leele.kafkadistributedchatserver.kafka.deserializer.ChatDeserializer");
-        KafkaConsumer<String, Chat> consumer = new KafkaConsumer<>(props);
 
-        consumer.subscribe(Collections.singletonList(topic));
+        List<Chat> chatList = new ArrayList<>();
+        Thread mainThread = Thread.currentThread();
+
+        KafkaConsumer<String, Chat> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(roomId));
+
+        // main 스레드 종료 시 별도 thread로 kafkaConsumer wakeup() 메서드를 호출
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                consumer.wakeup();
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         try {
             while (true) {
                 ConsumerRecords<String, Chat> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, Chat> record : records) {
-                    Chat chat = record.value();
-                    System.out.println("🫧Received message: " + chat.getMessage());
-                    // 여기서 처리 로직을 작성
+                    System.out.printf("🫧[Consumer] partition = %s, offset = %d, key = %s, value = %s %n",
+                            record.partition(), record.offset(), record.key(), record.value());
+
+                    chatList.add(record.value());
+                }
+
+                // Kafka에 저장된 모든 메시지 읽은 후 종료
+                if(!chatList.isEmpty() && records.isEmpty()) {
+                    sendDataToRestAPI(chatList, roomId, memberId);
+                    break;
                 }
             }
+        } catch (WakeupException e) {
+            System.out.println("❗️Wakeup exception has been called. " + e);
         } finally {
+            System.out.println("Finally consumer is closing...");
             consumer.close();
         }
     }
-}
 
+    @Autowired
+    public void StompMessageSender(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public static void sendDataToRestAPI(List<Chat> chat, String roomId, String memberId) {
+        messagingTemplate.convertAndSend("/sub/chat/init/" + roomId + memberId, chat);
+    }
+}
