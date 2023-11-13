5fc39be5792422056f3a9805b8bdb681a1129f1b
younglee-Choe <leele7111@naver.com>
2023-11-13 17:33:39 +0900

    serialize LocalDateTime to JSON

--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/kafka/serializer/ChatSerializer.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/kafka/serializer/ChatSerializer.java
@@ -2,11 +2,13 @@ package leele.kafkadistributedchatserver.kafka.serializer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import leele.kafkadistributedchatserver.chat.dto.Chat;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class ChatSerializer implements Serializer<Chat>, Serializable {
     @Override
@@ -16,6 +18,18 @@ public class ChatSerializer implements Serializer<Chat>, Serializable {
                 return null;
 
             ObjectMapper objectMapper = new ObjectMapper();
+
+            objectMapper.registerModule(new JavaTimeModule());
+
+            // LocalDateTime을 JSON으로 직렬화
+            try {
+                LocalDateTime localDateTime = LocalDateTime.now();
+                String json = objectMapper.writeValueAsString(localDateTime);
+                System.out.println(json);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+
             return objectMapper.writeValueAsBytes(data);
         } catch (JsonProcessingException e) {
             throw new SerializationException("Error when serializing Chat to byte[]");
