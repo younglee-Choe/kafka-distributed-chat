bc2d2e3bc25f8aeb684f1a06a293d10a2ec00ae3
younglee-Choe <leele7111@gmail.com>
2025-03-23 20:56:59 +0900

    HTTP requests between SpringBoot and FastAPI via webClient

--- kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/chat/client/ChatClient.java
+++ kafka-distributed-chat-server/src/main/java/leele/kafkadistributedchatserver/chat/client/ChatClient.java
@@ -1,40 +1,37 @@
 package leele.kafkadistributedchatserver.chat.client;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Service
 public class ChatClient {
     private final WebClient webClient;
+    private String topicName;
 
     public ChatClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8000").build(); // FastAPI 주소
+        this.webClient = webClientBuilder.baseUrl("http://127.0.0.1:8000").build(); // FastAPI 주소
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+        sendTopicNameToFastAPI(topicName).subscribe(
+                response -> System.out.println("✅FastAPI Response: " + response),
+                error -> System.err.println("❌FastAPI Transmission Error: " + error.getMessage())
+        );
+    }
+
+    public String getTopicName() {
+        return topicName;
     }
 
     public Mono<String> sendTopicNameToFastAPI(String topicName) {
         return webClient.post()
                 .uri("/chat/topic") // FastAPI endpoint
-                .bodyValue(new TopicName(topicName))
+                .bodyValue(Map.of("topicName", topicName))
                 .retrieve()
                 .bodyToMono(String.class);   // 응답을 String으로 변환
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    static class TopicName {
-        private String text;
-
-        public TopicName(String text) {
-            this.text = text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
-        }
     }
 }
